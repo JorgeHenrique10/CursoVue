2c19cf3c9b177a6030ac7b0e4a7641739e678d88
Jorge Henrique Jesus da SIlva <rick@MacBook-Pro-de-Jorge.local>
2021-10-05 06:57:04 -0300

    atualizacao

---
+++ 5-Componentes/componente-intro/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <h1>Contador</h1>
+    <app-contadores></app-contadores>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ 5-Componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="contadores">
+    <app-contador v-for="i in 5" :key="i"></app-contador>
+  </div>
+</template>
+
+<script>
+import Contador from "./Contador.vue";
+
+export default {
+  components: {
+    "app-contador": Contador,
+  },
+};
+</script>
+
+<style>
+</style>
